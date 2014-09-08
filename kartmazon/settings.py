be0e14c8ed94dfce83cf392a7d60a3ccb8f0421c
Mike <miguelbq88@gmail.com>
2014-09-07 21:01:38 -0700

    asd

--- kartmazon/settings.py
+++ kartmazon/settings.py
@@ -81,16 +81,16 @@ DATABASES = {
         'PORT': 5432,
         'USER': 'mulucakwetqfxl',
         'PASSWORD': 'xBEDTlOmsgOhAAZE2CHAOSyQcV'
-    },
-    'desarrollo': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'kartmazon',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'w4p1ll0',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
     }
+    # 'desarrollo': {
+    #     'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': 'kartmazon',                      # Or path to database file if using sqlite3.
+    #     # The following settings are not used with sqlite3:
+    #     'USER': 'root',
+    #     'PASSWORD': 'w4p1ll0',
+    #     'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #     'PORT': '',                      # Set to empty string for default.
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     #     'NAME': 'kartmazon',                      # Or path to database file if using sqlite3.
