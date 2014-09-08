1037be71f0ae65e7696e8b8df839cf798fcadb10
Mike <miguelbq88@gmail.com>
2014-09-07 20:14:33 -0700

    Cambio DB a postgre

--- kartmazon/settings.py
+++ kartmazon/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'kartmazon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'kartmazon',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
